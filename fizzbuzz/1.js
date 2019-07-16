48694fc2249eaf48587093c1efea37e9ca17b712
s17001 <s17001@std.it-college.ac.jp>
2019-07-16 19:34:02 +0900

    なんちゃって修正

--- fizzbuzz/1.js
+++ fizzbuzz/1.js
@@ -1,5 +1,9 @@
-const fizz = i => i%3?i:'fizz '
-const buzz = i => i%5?i:'buzz '
-const fizzbuzz = i => i%15?i:'fizz buzz'
+const fizz = i => i%3?i:typeof(i)=='string'?i:'fizz'
+const buzz = i => i%5?i:typeof(i)=='string'?i:'buzz'
+const fizzbuzz = i => i%15?i:typeof(i)=='string'?i:'fizz buzz'
 
-console.log(fizz(buzz(fizzbuzz(16))))
+for (let i = 1 ; i<16 ;i++){
+    console.log(
+        fizz(buzz(fizzbuzz(i)))
+        )
+}
