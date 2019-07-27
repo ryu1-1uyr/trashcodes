bdd40990ba35514d2bf75b5ea4d2d9d38024d4b8
s17001 <s17001@std.it-college.ac.jp>
2019-07-27 18:55:31 +0900

    a

--- Quine/index.js
+++ Quine/index.js
@@ -1,1 +1,34 @@
-console.log((_=()=>('puts(%[<?php echo( ]+%(console.log((_='+_+')())).inspect+%[)?>])'))())
+console.log(
+    (_=()=>('puts(%[<?php echo( ]+%(console.log((_='+_+')())).inspect+%[)?>])'))()
+    )
+
+(_=()=>('('+_+')()'))()
+/*
+('puts( 
+    %[<?php echo( ] + %(console.log((_='+_+')())).inspect + %[)?>]
+    )')
+
+    %[<?php echo( 
+        ] + %(console.log((_='+_+')())).inspect + %[
+            )?>]
+
+        ] + %(
+            console.log((_='+_+')()))
+            .inspect + %[
+
+*/
+
+(_ = (() => {console.log('_ =('+_+')()')}))()
+(_=()=>('(_='+_+')()'))()
+
+(_=()=>console.log('(_='+_+')()'))()
+
+
+
+(_=()=>console.log(
+    `(_='+${_}+')()`
+    ))()
+
+
+(_=()=>console.log(`puts'(_='+${_}+')()'`))()
+    
