a2d8b30508ad0308b8b7cde56d1fa4aba919e858
s17001 <s17001@std.it-college.ac.jp>
2019-07-27 18:21:43 +0900

    add fire

---
+++ Quine/Untitled-1.rb
@@ -0,0 +1,4 @@
+puts(%(<?php echo(#{%(console.log((_=()=>('puts(%(<?php echo(#{%(console.log((_='+_+')())).inspect}) ?> ) )' ))())).inspect}) ?> ) )
+puts(%[ <?php echo( ]+%(console.log((_=()=>('puts(%[ <?php echo( ]+%(console.log((_='+_+')())).inspect + %[ )?> ])' ))())).inspect + %[ )?> ])
+
+%[]+%[].inspect+%[]
