069cd54dd725cbda6d13ffc127872643eb76c57f
s17001 <s17001@std.it-college.ac.jp>
2019-07-27 18:21:05 +0900

    edit

--- fizzbuzz/1.js
+++ fizzbuzz/1.js
@@ -1,9 +1,9 @@
-const fizz = i => i%3?i:typeof(i)=='string'?i:'fizz'
-const buzz = i => i%5?i:typeof(i)=='string'?i:'buzz'
-const fizzbuzz = i => i%15?i:typeof(i)=='string'?i:'fizz buzz'
+const f = i => i%3?i:typeof(i)=='string'?i:'fizz'
+const b = i => i%5?i:typeof(i)=='string'?i:'buzz'
+const fb = i => i%15?i:typeof(i)=='string'?i:'fizz buzz'
 
 for (let i = 1 ; i<16 ;i++){
     console.log(
-        fizz(buzz(fizzbuzz(i)))
+        f(b(fb(i)))
         )
 }
