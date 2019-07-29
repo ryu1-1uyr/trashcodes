92565a483d413c072216a25f1e8e91322aa6dc74
s17001 <s17001@std.it-college.ac.jp>
2019-07-29 23:20:53 +0900

    bfするJSのコードの雛形作った

---
+++ Quine/bf.js
@@ -0,0 +1,58 @@
+// def bfeval code
+//   array = []
+//   idx = 0
+//   output = []
+//   eval code.scan(/[\[\].,><+-]/).map(&{
+//     '[' => 'while array[idx].to_i != 0',
+//     ']' => 'end',
+//     '.' => 'output<<array[idx].to_i.chr',
+//     ',' => 'array[idx] = getc.ord',
+//     '+' => 'array[idx] = array[idx].to_i + 1',
+//     '-' => 'array[idx] = array[idx].to_i - 1',
+//     '>' => 'idx+=1',
+//     '<' => 'idx-=1'
+//   }).join(';')
+//   output.join
+// end
+
+const bfeval = (code) => {
+    array = [... new Array(code.length).keys()]
+    idx = 0 
+    output = []
+    sampleOutput = `array = [... new Array(${code.length}).keys()];idx = 0;output = [];`
+    // code.replace(/[\[\].,><+-]/)
+    input = [... code].map(x => {
+        // console.log(x)
+        switch (x) {
+            case '[':
+                sampleOutput+=('while(array[idx] != 0){console.log(array[idx] , 0);')
+                break
+            case ']':
+                sampleOutput+=('};')
+                break
+            case '.':
+                sampleOutput+=('output.join(array[idx]);console.log( (array[idx]-[]) );')
+                break
+            case ',':
+                sampleOutput+=(`array[idx] = ${x};`)
+                break
+            case '+':
+                sampleOutput+=('array[idx] = (array[idx]-[]) + 1;')
+                break
+            case '-':
+                sampleOutput+=('array[idx] = (array[idx]-[]) - 1;')
+                break
+            case '>':
+                sampleOutput+=('idx+=1;')
+                break
+            case '<':
+                sampleOutput+=('idx-=1;')
+                break
+            default:
+
+        }
+        // console.log(sampleOutput)
+    })
+    return sampleOutput
+}
+console.log(bfeval('+++++++++[>++++++++>+++++++++++>+++>+<<<<-]>.>++.+++++++..+++.>+++++.<<+++++++++++++++.>.+++.------.--------.>+.>+.'))
